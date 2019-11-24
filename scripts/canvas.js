39dd6cc43126c794687e6d70d3e225e8ed5aa94a
well3112 <lance.a.wells@gmail.com>
2019-11-23 19:15:11 -0800

    Updated canvas to use promises technique for guaranteed load order.

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -1,19 +1,42 @@
+/**
+ * @description
+ * Creates a promise on loading an image. Borrowed from an answer to this this StackOverflow question:
+ * https: //stackoverflow.com/questions/47579213/draw-image-to-canvas-in-particular-order-while-loading-from-url-to-image-object
+
+ * @param {String} imagePath The relative location of the image to be loaded. On a failure, this promise will
+ * fail to resolve. On a success, the promise will resolve.
+
+ * @returns {Promise} A promise on the resolution to an image being loaded.
+ */
+function LoadImage(imagePath) {
+    return new Promise((resolve, reject) => {
+        let image = new Image();
+        image.addEventListener("load", () => {
+            resolve(image);
+        });
+        image.addEventListener("error", (err) => {
+            reject(err);
+        });
+        image.src = imagePath;
+    });
+}
+
 function draw() {
     var canvas = document.getElementById('tutorial');
     if (canvas.getContext('2d')) {
         var ctx = canvas.getContext('2d');
+        var imageSources =
+        [
+            "images/CharacterCreator/BaseBody/Purple.png",
+            "images/CharacterCreator/Accessories/WorldsWorstHat.png",
+        ];
 
-        var baseImage = new Image();
-        baseImage.src = "images/CharacterCreator/BaseBody/Purple.png";
-        baseImage.onload = function()
-        {
-            ctx.drawImage(baseImage, 0, 0);
-        }
-
-        var accImage = new Image();
-        accImage.src = "images/CharacterCreator/Accessories/WorldsWorstHat.png";
-        accImage.onload = function () {
-            ctx.drawImage(accImage, 0, 0);
-        }
+        Promise
+            .all(imageSources.map(i => LoadImage(i)))
+            .then((images) => {
+                images.forEach((image) => {
+                    ctx.drawImage(image, 0, 0);
+                })
+            })
     }
 }
