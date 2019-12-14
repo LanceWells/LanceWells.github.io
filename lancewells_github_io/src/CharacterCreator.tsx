c223798add8a50de560749a30507e75e4099350d
well3112 <lance.a.wells@gmail.com>
2019-12-13 23:52:58 -0800

    Another reorg for images and added valkyrie set.

--- lancewells_github_io/src/CharacterCreator.tsx
+++ lancewells_github_io/src/CharacterCreator.tsx
@@ -8,11 +8,9 @@ import {PartAccordion} from './PartAccordion';
 import {CharacterCanvas} from './CharacterCanvas';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Accordion from 'react-bootstrap/Accordion';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import CardColumns from 'react-bootstrap/CardColumns'
 
 /**
  * @description
