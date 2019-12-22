d050d06c869c7242870ef242494622864d8a2baf
well3112 <lance.a.wells@gmail.com>
2019-12-22 11:21:43 -0800

    Floating rings and more tests.

--- lancewells_github_io/src/ShopItem.tsx
+++ lancewells_github_io/src/ShopItem.tsx
@@ -8,10 +8,24 @@ export function ShopItem(props: IShopItemProps) {
     return (
         <div className="shop-item">
             <Button variant='link'>
-                <img className='item-layer' src='./images/Item_Shop/Items/Rings/gold_simple.gif' />
-                <img className='item-layer' src='./images/Item_Shop/Items/Rings/gold_standard_jewel_red.gif' />
-                <img className='item-layer' src='./images/Item_Shop/Items/Rings/silver_snake_flower_blue.gif' />
-                <img className='item-layer' src='./images/Item_Shop/Items/Rings/silver_standard_jewel_green.gif' />
+                <div className='item-layer' style={{animationDelay: '0s'}}>
+                    <img src='./images/Item_Shop/Items/Rings/Ring Jewel Red.png'/>
+                </div>
+            </Button>
+            <Button variant='link'>
+                <div className='item-layer' style={{ animationDelay: '1s' }}>
+                    <img src='./images/Item_Shop/Items/Rings/Ring Silver Jewel Green.png' />
+                </div>
+            </Button>
+            <Button variant='link'>
+                <div className='item-layer' style={{ animationDelay: '2s' }}>
+                    <img src='./images/Item_Shop/Items/Rings/Ring Silver Snake Flower Blue.png' />
+                </div>
+            </Button>
+            <Button variant='link'>
+                <div className='item-layer' style={{ animationDelay: '3s' }}>
+                    <img src='./images/Item_Shop/Items/Rings/Ring Simple.png' />
+                </div>
             </Button>
         </div>
     )
