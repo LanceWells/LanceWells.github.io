6920e362597726670149c1c629606da8b7ed46e0
well3112 <lance.a.wells@gmail.com>
2019-12-22 12:34:40 -0800

    Minor changes and a functional modal.

--- lancewells_github_io/src/ItemShop.tsx
+++ lancewells_github_io/src/ItemShop.tsx
@@ -1,22 +1,51 @@
 import './css/ItemShop.css';
 import React from 'react';
 
-import {ShopItem} from './ShopItem';
+import { ShopItem } from './ShopItem';
+import { Modal, ModalTitle } from 'react-bootstrap';
 
 interface IItemShopProps {
 };
 
 interface IItemShopState {
+    showItemDialog: boolean;
+    itemDialogTitle: string;
+    itemDialogBody: string;
 };
 
 export class ItemShop extends React.Component<IItemShopProps, IItemShopState> {
     constructor (props: IItemShopProps) {
         super(props);
         this.state = {
+            showItemDialog: false,
+            itemDialogTitle: 'test title',
+            itemDialogBody: 'test body',
         };
     }
 
+    setModalVisiblity(show: boolean)
+    {
+        this.setState({
+            showItemDialog: show
+        });
+    }
+
+    onItemClick(itemTitle: string)
+    {
+        this.setState({
+            itemDialogTitle: itemTitle,
+        });
+        
+        this.setModalVisiblity(true);
+    }
+
     render() {
+        /* Keep these as consts because if we were to use a function callback when closing the Modal,
+         * that would result in an exception (because we're then in a state that doesn't recognize)
+         * ItemShop as 'this'. */
+        const hideModal = () => this.setModalVisiblity(false);
+        const showModal = () => this.setModalVisiblity(true);
+
         return (
             <div className="ItemShop">
                 <h1>Item Shop</h1>
@@ -30,39 +59,65 @@ export class ItemShop extends React.Component<IItemShopProps, IItemShopState> {
                         <ShopItem 
                             imageSource='./images/Item_Shop/Items/Rings/Ring Jewel Red.png'
                             floatDelay={0}
+                            itemName='Ring Jewel Red'
+                            onItemClick={(itemTitle: string) => this.onItemClick(itemTitle)}
                         />
                         <ShopItem
                             imageSource='./images/Item_Shop/Items/Rings/Ring Silver Jewel Green.png'
                             floatDelay={-1}
+                            itemName='Ring Silver Jewel Green'
+                            onItemClick={(itemTitle: string) => this.onItemClick(itemTitle)}
                         />
                         <ShopItem
                             imageSource='./images/Item_Shop/Items/Rings/Ring Silver Snake Flower Blue.png'
                             floatDelay={-2}
+                            itemName='Ring Silver Snake Flower Blue'
+                            onItemClick={(itemTitle: string) => this.onItemClick(itemTitle)}
                         />
                         <ShopItem
                             imageSource='./images/Item_Shop/Items/Rings/Ring Simple.png'
                             floatDelay={-3}
+                            itemName='Ring Simple'
+                            onItemClick={(itemTitle: string) => this.onItemClick(itemTitle)}
                         />
                     </div>
                     <div className='shop-rug red-rug'>
                         <ShopItem
                             imageSource='./images/Item_Shop/Items/Rings/Ring Jewel Red.png'
                             floatDelay={0}
+                            itemName='Ring Jewel Red'
+                            onItemClick={(itemTitle: string) => this.onItemClick(itemTitle)}
                         />
                         <ShopItem
                             imageSource='./images/Item_Shop/Items/Rings/Ring Silver Jewel Green.png'
                             floatDelay={-1}
+                            itemName='Ring Silver Jewel Green'
+                            onItemClick={(itemTitle: string) => this.onItemClick(itemTitle)}
                         />
                         <ShopItem
                             imageSource='./images/Item_Shop/Items/Rings/Ring Silver Snake Flower Blue.png'
                             floatDelay={-2}
+                            itemName='Ring Silver Snake Flower Blue'
+                            onItemClick={(itemTitle: string) => this.onItemClick(itemTitle)}
                         />
                         <ShopItem
                             imageSource='./images/Item_Shop/Items/Rings/Ring Simple.png'
                             floatDelay={-3}
+                            itemName='Ring Simple'
+                            onItemClick={(itemTitle: string) => this.onItemClick(itemTitle)}
                         />
                     </div>
                 </div>
+                <Modal show={this.state.showItemDialog} onHide={hideModal} centered={true}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>
+                            {this.state.itemDialogTitle}
+                        </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        {this.state.itemDialogBody}
+                    </Modal.Body>
+                </Modal>
             </div>
         );
     }
--- lancewells_github_io/src/ShopItem.tsx
+++ lancewells_github_io/src/ShopItem.tsx
@@ -1,19 +1,29 @@
 import React from 'react';
-import {Button} from 'react-bootstrap';
+import {Button, OverlayTrigger, Tooltip} from 'react-bootstrap';
 
 interface IShopItemProps {
     imageSource: string;
+    itemName: string;
     floatDelay: number;
+    onItemClick: Function;
 };
 
 export function ShopItem(props: IShopItemProps) {
-    const animationDelay = props.floatDelay;
-
     return (
-        <Button variant='link'>
-            <div className='item-layer' style={{animationDelay: `${animationDelay}s`}}>
-                <img src={props.imageSource}/>
-            </div>
-        </Button>
+        <OverlayTrigger
+            placement='top'
+            delay={{ show: 0, hide: 400 }}
+            overlay={
+                <Tooltip id='item-tooltip'>
+                    {props.itemName}
+                </Tooltip>
+            }
+            >
+            <Button variant='link' onClick={() => props.onItemClick(props.itemName)}>
+                <div className='item-layer' style={{ animationDelay: `${props.floatDelay}s` }}>
+                    <img src={props.imageSource} />
+                </div>
+            </Button>
+        </OverlayTrigger>
     )
 }
