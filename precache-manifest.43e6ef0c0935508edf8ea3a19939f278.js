5b10f6a7caa5137fceecf5c01dc695af32477d68
well3112 <lance.a.wells@gmail.com>
2019-12-31 17:42:53 -0800

    Updates

---
+++ precache-manifest.43e6ef0c0935508edf8ea3a19939f278.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bfc1d7fead4f3be04f0af787dbc62e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3cbbd57ca073b0e78ae",
+    "url": "/static/css/2.7aa6f31f.chunk.css"
+  },
+  {
+    "revision": "c7437676374bbd63c9b1",
+    "url": "/static/css/main.f275b4b9.chunk.css"
+  },
+  {
+    "revision": "d3cbbd57ca073b0e78ae",
+    "url": "/static/js/2.0cef4fdf.chunk.js"
+  },
+  {
+    "revision": "54f6e2d0ef339619d0a432be7ac0ab75",
+    "url": "/static/js/2.0cef4fdf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c7437676374bbd63c9b1",
+    "url": "/static/js/main.99d5d4d6.chunk.js"
+  },
+  {
+    "revision": "c804ab348e7efe8f628a",
+    "url": "/static/js/runtime-main.05cd634c.js"
+  }
+]);
