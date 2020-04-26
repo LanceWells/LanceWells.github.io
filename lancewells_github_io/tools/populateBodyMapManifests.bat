a99bece10a99c494c508dde335168f37a9958fc3
well3112 <lance.a.wells@gmail.com>
2020-04-25 21:27:51 -0700

    Removed old character creator files.

--- lancewells_github_io/tools/populateBodyMapManifests.bat
+++
@@ -1,7 +0,0 @@
-REM Humanoid
-BodyMapGenerator.exe .\..\public\images\Size_Average .\..\src\json\avg_Feminine.json ./images/ Humanoid_Female Humanoid_Androgynous Female Androgynous
-BodyMapGenerator.exe .\..\public\images\Size_Average .\..\src\json\avg_Masculine.json ./images/ Humanoid_Male Humanoid_Androgynous Male Androgynous
-
-REM Reptilian/Dragonborn
-BodyMapGenerator.exe .\..\public\images\Size_Average .\..\src\json\avg_Feminine_Reptilian.json ./images/ Reptilian_Female Reptilian_Androgynous Female Androgynous
-BodyMapGenerator.exe .\..\public\images\Size_Average .\..\src\json\avg_Masculine_Reptilian.json ./images/ Reptilian_Male Reptilian_Androgynous Male Androgynous
