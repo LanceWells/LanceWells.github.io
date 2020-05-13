a25884825508160b621c1feacd580b18419103e2
well3112 <lance.a.wells@gmail.com>
2020-05-12 22:50:29 -0700

    Moved inventory component to a functional component.

--- lancewells_github_io/src/Inventory/React/Inventory.tsx
+++ lancewells_github_io/src/Inventory/React/Inventory.tsx
@@ -1,7 +1,6 @@
 import '../Inventory.css';
 
-import React from 'react';
-import { CharacterStateManager } from '../../FirebaseAuth/Classes/CharacterStateManager';
+import React, { useState, useEffect } from 'react';
 import { IItem } from '../../ItemData/Interfaces/IItem';
 import { ItemSource } from '../../ItemData/Classes/ItemSource';
 import { ItemDetailsModal } from '../../ItemData/React/ItemDetailsModal';
@@ -13,178 +12,157 @@ import { InventoryTab } from './InventoryTab';
 import { Tabs, Tab } from 'react-bootstrap';
 import { LoadingPlaceholder } from '../../Utilities/React/LoadingPlaceholder';
 import { LoginState } from '../../LoginPage/Enums/LoginState';
-import { PlayerCharacterData } from '../../FirebaseAuth/Types/PlayerCharacterData';
-import { UserDataAuth } from '../../FirebaseAuth/Classes/UserDataAuth';
-
-enum LoadingState {
-    Loading,
-    Loaded,
-    Anonymous,
-    NoCharacters
-}
+import { LoadingState } from '../../Utilities/Enums/LoadingState';
+import { useLoadingState } from '../../Utilities/Hooks/useLoadingState';
+import { useCharData } from '../../Utilities/Hooks/useCharData';
+import { ItemClick } from '../../ItemData/Types/CardButtonCallbackTypes/ItemClick';
+import { AttackClick } from '../../ItemData/Types/CardButtonCallbackTypes/AttackClick';
 
 export interface IInventoryProps {
     loginState: LoginState;
 }
 
-export interface IInventoryState {
-    items: IItem[];
-    showItemDetails: boolean;
-    focusedItem: IItem;
-    showAttackWindow: boolean;
-    attackName: string;
-    attacks: Attack[];
-    activeTab: string;
-    loadingState: LoadingState;
+export function Inventory(props: IInventoryProps) {
+    /**
+     * A series of items that will be rendered by this class. This should come entirely from the loaded
+     * character data.
+     */
+    const [items, setItems] = useState<IItem[]>([]);
+
+    /**
+     * A boolean value informing the item details window when to be visible.
+     */
+    const [showItemDetails, setShowItemDetails] = useState(false);
+
+    /**
+     * A boolean value indicating what to use as the item to display in the item details window.
+     */
+    const [focusedItem, setFocusedItem] = useState<IItem>(new ItemWondrous());
+
+    /**
+     * A boolean value informing the attack window when to be visible.
+     */
+    const [showAttackWindow, setShowAttackWindow] = useState(false);
+
+    /**
+     * The attack name that will be displayed in the attack window.
+     */
+    const [attackName, setAttackName] = useState("");
+
+    /**
+     * A series of attacks to get damage values for whem the attack window is open. Note that these are not
+     * separate actions, but separate damage values.
+     */
+    const [attacks, setAttacks] = useState<Attack[]>([]);
+
+    /**
+     * The active tab name. Required for the bootstrap component.
+     */
+    const [activeTab, setActiveTab] = useState(ItemType.Weapon.toString());
+
+    const loadingState = useLoadingState(props.loginState);
+    const charData = useCharData(loadingState);
+
+    /**
+     * Gets the user's items from a common source. Only adds items that have a discrete value. Only runs when
+     * the user's character data updates.
+     */
+    useEffect(() => {
+        let newItems: IItem[] = [];
+
+        if (charData) {
+            charData.Items.forEach(item => {
+                let foundItem: IItem | undefined = ItemSource.GetItem(item.key, item.type);
+
+                if (foundItem) {
+                    newItems.push(foundItem);
+                }
+            });
+        }
+
+        setItems(newItems);
+    }, [charData]);
+
+    function HandleHideDetails() {
+        setShowItemDetails(false);
+    }
+    
+    function HandleHideAttack() {
+        setShowAttackWindow(false);
+    }
+
+    function HandleTabSelection(key: string): void {
+        setActiveTab(key);
+    }
+
+    function HandleItemClick(item: IItem): void {
+        setFocusedItem(item);
+        setShowItemDetails(true);
+    }
+
+    function HandleAttackClick(attackName: string, attackRolls: Attack[]): void {
+        setAttackName(attackName);
+        setAttacks(attackRolls);
+        setShowAttackWindow(true);
+    }
+
+    return (
+        <div className="inventory-container">
+            <LoadingPlaceholder
+                showSpinner={loadingState === LoadingState.Loading}
+                role="Inventory Loading Status">
+                <ItemDetailsModal
+                    show={showItemDetails}
+                    hideModal={HandleHideDetails}
+                    itemDetails={focusedItem}
+                />
+                <AttackRollModal
+                    show={showAttackWindow}
+                    attackName={attackName}
+                    attacks={attacks}
+                    onHide={HandleHideAttack}
+                />
+                <div className='inventory-tabs-container'>
+                    <Tabs
+                        id="Inventory Tabs"
+                        activeKey={activeTab.toString()}
+                        onSelect={HandleTabSelection}
+                    >
+                        {GetInventoryTabs(items, HandleItemClick, HandleAttackClick)}
+                    </Tabs>
+                </div>
+            </LoadingPlaceholder>
+        </div>
+    );
 }
 
-export class Inventory extends React.Component<IInventoryProps, IInventoryState> {
-    public constructor(props: IInventoryProps) {
-        super(props);
-        this.state = {
-            items: [],
-            showItemDetails: false,
-            focusedItem: new ItemWondrous(),
-            showAttackWindow: false,
-            attackName: "",
-            attacks: [],
-            activeTab: ItemType.Weapon.toString(),
-            loadingState: LoadingState.Loading,
-        };
+/**
+ * Gets a tabbed component for each item type that will exist in the inventory.
+ * @param items The entire series of items for the current character.
+ * @param handleItemClick Handles when a user clicks for an item display.
+ * @param handleAttackClick Handles when a user clicks an attack button for an item.
+ */
+function GetInventoryTabs(items: IItem[], handleItemClick: ItemClick, handleAttackClick: AttackClick): JSX.Element[] {
+    let itemTabs: JSX.Element[] = Object.values(ItemType).map(itemType => {
+        let filteredItems: IItem[] = items.filter(item => item.type === itemType);
 
-        this.UpdateItems();
-        CharacterStateManager.GetInstance().AddObserver(this.characterStateManager_NotifyObservers.bind(this))
-    }
-
-    public componentDidUpdate(prevProps: IInventoryProps): void {
-        if (this.props.loginState !== prevProps.loginState || this.state.loadingState == LoadingState.NoCharacters) {
-            // basically, use this to handle login or logout events. otherwise, just load what we have. the
-            // auto-login is messing with using only this.
-            this.UpdateItems();
-        }
-    }
-
-    public characterStateManager_NotifyObservers(charData: PlayerCharacterData | undefined): void {
-        this.UpdateItems();
-    }
-
-    private async UpdateItems() {
-        let newItems: IItem[] = [];
-        let loadingState: LoadingState = LoadingState.Anonymous;
-        let userHasAccess: boolean = await UserDataAuth.GetInstance().CheckForAccess();
-
-        if (userHasAccess) {
-            let staticCharData: PlayerCharacterData | undefined = undefined; 
-            staticCharData = await CharacterStateManager.GetInstance().GetCharacter();
-            
-            if (staticCharData === undefined) {
-                loadingState = LoadingState.NoCharacters;
-            }
-            else {
-                staticCharData.Items.forEach(item => {
-                    let foundItem: IItem | undefined = ItemSource.GetItem(item.key, item.type);
-
-                    if (foundItem) {
-                        newItems.push(foundItem);
-                    }
-                });
-
-                loadingState = LoadingState.Loaded;
-            }
-        }
-
-        this.setState({
-            items: newItems,
-            loadingState: loadingState,
-        });
-    }
-
-    private ShowItemDetails(itemJson: IItem): void {
-        this.setState({
-            showItemDetails: true,
-            focusedItem: itemJson
-        })
-    }
-
-    private HideItemDetails() {
-        this.setState({
-            showItemDetails: false
-        })
-    }
-
-    private ShowAttackWindow(attackName: string, attackRolls: Attack[]): void {
-        this.setState({
-            showAttackWindow: true,
-            attackName: attackName,
-            attacks: attackRolls
-        });
-    }
-
-    private HideAttackWindow(): void {
-        this.setState({
-            showAttackWindow: false
-        });
-    }
-
-    private HandleTabSelection(key: string): void {
-        this.setState({
-            activeTab: key
-        })
-    }
-
-    private GetInventoryTabs(): JSX.Element[] {
-        let itemTabs: JSX.Element[] = Object.values(ItemType).map(itemType => {
-            let filteredItems: IItem[] = this.state.items.filter(item => item.type === itemType);
-
-            return (
-                <Tab eventKey={itemType.toString()}
-                    title={
-                        <div>
-                            <img className="inventory-tab-icon" src={`./images/Inventory/Tab_${itemType}.png`}/>
-                            <span>{`${itemType} (${filteredItems.length})`}</span>
-                        </div>
-                    }>
-                    <InventoryTab
-                        items={filteredItems}
-                        itemType={itemType}
-                        itemClick={this.ShowItemDetails.bind(this)}
-                        attackClick={this.ShowAttackWindow.bind(this)}
-                    />
-                </Tab>
-            )
-        });
-
-        return itemTabs;
-    }
-
-    public render() {
         return (
-            <div className="inventory-container">
-                <LoadingPlaceholder
-                    showSpinner={this.state.loadingState === LoadingState.Loading}
-                    role="Inventory Loading Status">
-                    <ItemDetailsModal
-                        show={this.state.showItemDetails}
-                        hideModal={this.HideItemDetails.bind(this)}
-                        itemDetails={this.state.focusedItem}
-                    />
-                    <AttackRollModal
-                        show={this.state.showAttackWindow}
-                        attackName={this.state.attackName}
-                        attacks={this.state.attacks}
-                        onHide={this.HideAttackWindow.bind(this)}
-                    />
-                    <div className='inventory-tabs-container'>
-                        <Tabs
-                            id="Inventory Tabs"
-                            activeKey={this.state.activeTab.toString()}
-                            onSelect={this.HandleTabSelection.bind(this)}
-                        >
-                            {this.GetInventoryTabs()}
-                        </Tabs>
+            <Tab eventKey={itemType.toString()}
+                title={
+                    <div>
+                        <img className="inventory-tab-icon" src={`./images/Inventory/Tab_${itemType}.png`}/>
+                        <span>{`${itemType} (${filteredItems.length})`}</span>
                     </div>
-                </LoadingPlaceholder>
-            </div>
+                }>
+                <InventoryTab
+                    items={filteredItems}
+                    itemType={itemType}
+                    itemClick={handleItemClick}
+                    attackClick={handleAttackClick}
+                />
+            </Tab>
         )
-    }
+    });
+
+    return itemTabs;
 }
